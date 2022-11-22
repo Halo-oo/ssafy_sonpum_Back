c01cb5e1d2ce1b5a964ac7b91696cf1f34b18468
Halo <87004079+Halo-oo@users.noreply.github.com>
2022-11-22 09:08:23 +0900

    UPDATE: 시세 목록 조회컬럼 추가
    아파트 이름, 건물 본번호/부번호 추가 조회

--- Back-end/VueApi/src/main/java/com/ssafy/vue/model/HouseDealInfoDto.java
+++ Back-end/VueApi/src/main/java/com/ssafy/vue/model/HouseDealInfoDto.java
@@ -28,91 +28,97 @@ public class HouseDealInfoDto {
 	private String area;
 	@ApiModelProperty(value = "층")
 	private String floor;
+	@ApiModelProperty(value = "도로명")
+	private String roadName;
+	@ApiModelProperty(value = "건물 본 번호")
+	private String roadNameBonbun;
+	@ApiModelProperty(value = "건물 부 번호")
+	private String roadNameBubun;
 	
 	public BigInteger getAddressId() {
 		return addressId;
 	}
-	
 	public void setAddressId(BigInteger addressId) {
 		this.addressId = addressId;
 	}
-	
 	public String getApartName() {
 		return apartName;
 	}
-	
 	public void setApartName(String apartName) {
 		this.apartName = apartName;
 	}
-	
 	public String getDongCode() {
 		return dongCode;
 	}
-	
 	public void setDongCode(String dongCode) {
 		this.dongCode = dongCode;
 	}
-	
 	public String getBuildYear() {
 		return buildYear;
 	}
-	
 	public void setBuildYear(String buildYear) {
 		this.buildYear = buildYear;
 	}
-	
 	public String getDealAmount() {
 		return dealAmount;
 	}
-	
 	public void setDealAmount(String dealAmount) {
 		this.dealAmount = dealAmount;
 	}
-	
 	public String getDealYear() {
 		return dealYear;
 	}
-	
 	public void setDealYear(String dealYear) {
 		this.dealYear = dealYear;
 	}
-	
 	public int getDealMonth() {
 		return dealMonth;
 	}
-	
 	public void setDealMonth(int dealMonth) {
 		this.dealMonth = dealMonth;
 	}
-	
 	public String getDealDay() {
 		return dealDay;
 	}
-	
 	public void setDealDay(String dealDay) {
 		this.dealDay = dealDay;
 	}
-	
 	public String getArea() {
 		return area;
 	}
-	
 	public void setArea(String area) {
 		this.area = area;
 	}
-	
 	public String getFloor() {
 		return floor;
 	}
-	
 	public void setFloor(String floor) {
 		this.floor = floor;
+	}
+	public String getRoadName() {
+		return roadName;
+	}
+	public void setRoadName(String roadName) {
+		this.roadName = roadName;
+	}
+	public String getRoadNameBonbun() {
+		return roadNameBonbun;
+	}
+	public void setRoadNameBonbun(String roadNameBonbun) {
+		this.roadNameBonbun = roadNameBonbun;
+	}
+	public String getRoadNameBubun() {
+		return roadNameBubun;
+	}
+	public void setRoadNameBubun(String roadNameBubun) {
+		this.roadNameBubun = roadNameBubun;
 	}
 	
 	@Override
 	public String toString() {
 		return "HouseDealInfoDto [addressId=" + addressId + ", apartName=" + apartName + ", dongCode=" + dongCode
 				+ ", buildYear=" + buildYear + ", dealAmount=" + dealAmount + ", dealYear=" + dealYear + ", dealMonth="
-				+ dealMonth + ", dealDay=" + dealDay + ", area=" + area + ", floor=" + floor + "]";
+				+ dealMonth + ", dealDay=" + dealDay + ", area=" + area + ", floor=" + floor + ", roadName=" + roadName
+				+ ", roadNameBonbun=" + roadNameBonbun + ", roadNameBubun=" + roadNameBubun + "]";
 	}
 }
